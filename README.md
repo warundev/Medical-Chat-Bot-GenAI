5cd00a18e5eebac40412f1af3220c057ed5f26dc
warundev <120333797+warundev@users.noreply.github.com>
2025-02-13 09:37:31 +0530

    floder structure added

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Medical-Chat-Bot-GenAI
+
+
+### Step 01  - Create a conda environment
+
+```bash
+conda create -n medibot python=3.10 -y
+```
+
+```bash
+conda activate medibot
+```
+
+
+### Step 02 - install the requirement
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= 'Generative AI Medical Chatbot',
+    version= '0.0.1',
+    author= 'Waruna Liyanapathirana',
+    author_email= 'send2liyanapathirana@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+)
